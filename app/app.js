f8f1f59856a6ea149271752e5b78856c1305a6c6
akmal <akmal@brighter.com>
2014-10-28 11:15:45 -0700

    -a

--- app/app.js
+++ app/app.js
@@ -1,3 +1,7 @@
-angular.module('calendarDemoApp', []);
+angular.module('calendarDemoApp', [])
+
+.controller("demoController", function($scope){
+	$scope.title = "Calendar directive";
+});
 
 // your controller and directive code go here
